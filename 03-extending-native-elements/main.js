f29aaeaf277da5d7fec3cfd14015921df269f002
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-12-27 17:24:22 +0100

    Include the polyfill document-register-element.js from WebReflection repository

--- 03-extending-native-elements/main.js
+++ 03-extending-native-elements/main.js
@@ -1,5 +1,14 @@
+// 1. To create this custom element we're using a classical CustomAnchor which instead of extending HTML element, we're extending the HTML anchor element. First 
+
 class CustomAnchor extends HTMLAnchorElement {
     connectedCallBack() {
-        
+        this.addEventListener('click', ev => {
+            ev.preventDefault();
+            const result = confirm('Estás segura de que quieres dejar mi página para navegar por San Google?');
+            if (result) {
+                window.location.href = ev.target.href;
+            }
+        });
     }
 }
+window.customElements.define('custom-anchor', CustomAnchor, { extends: 'a' });
