bd01d38b7f651163025ac234c9b1eba0229ad67b
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-12-28 16:21:49 +0100

    Clone the contents of the template & and render the list using appenchild

---
+++ 05-using-html-templates/main.js
@@ -0,0 +1,6 @@
+
+// HTML templates give us the ability to create reusable segments of HTML within our apps.
+
+// The <template> element is not automatically rendered by the browser.
+
+// Templates can be used to define markup for specific segments of an application and then rendered on-the-fly as required.
