956a9519d8130cd4313847e50f36b25ad6c4cfef
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-12-28 17:37:25 +0100

    Finish exercise

--- 01-web-component-specifications/06-using-html-imports/main.js
+++ 01-web-component-specifications/06-using-html-imports/main.js
@@ -7,3 +7,15 @@
 // Unfortunately, though, the HTML import specification is a contentious one owed to some browsers made the decision not to implement this spec and wait for the outcome of the JavaScript module spcification
 
 // Although the HTML imports feature is very easy to polyfill 
+
+// 2. Let's import the content of "notice.html" and make them available to us by selecting the link we've just added
+
+const $link = document.getElementById('notice');
+
+// 3. Get the content of that import
+const content = $link.import.querySelector('.notice-frame');
+
+// 4. Grab the notice frame and add the entirety of the notice frame to the body of index.html
+document.body.appendChild(content.cloneNode(true));
+
+// 5. Now if we open the browser we'll see that the contents of notice.html has been added to the main document
