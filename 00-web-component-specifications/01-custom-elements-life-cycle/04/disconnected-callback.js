92179c41a4ff7ac17852cfb22cc8d9c37117ae33
elena valenzuela <elena.valenzuela.guerrero@gmail.com>
2020-12-29 16:52:45 +0100

    Update disconnected-callback.js

--- 00-web-component-specifications/01-custom-elements-life-cycle/04/disconnected-callback.js
+++ 00-web-component-specifications/01-custom-elements-life-cycle/04/disconnected-callback.js
@@ -7,14 +7,12 @@ class MyCustomElement extends HTMLElement {
   // 3rd Callback
 
   // 1. This method is executed when the element is removed from the DOM. This method is used usually to do some clean up, for example, canceling any JavaScript intervals you have running
-
 disconnectedCallback();
 {
     console.log('My custom Element removed from the DOM');
 }
 
   // This code is just an example to see how this callback method works. We're creating an instance of our MyCustomElement
-
 let $ce = document.createElement('my-custom-element');
 setTimeout(() => {
     // Add our Custom Element to the DOM
