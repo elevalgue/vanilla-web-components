83fba02df0962820b9867da2ce33687032222214
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-12-30 15:29:07 +0100

    Comment on code

--- 01-basic-anatomy-of-a-web-component/05-web-component-shadow-dom/rw-poll.js
+++ 01-basic-anatomy-of-a-web-component/05-web-component-shadow-dom/rw-poll.js
@@ -10,50 +10,61 @@ class RwPoll extends HTMLElement {
         this._$answers = null;
 
         // 2. When we introduce the shadow DOM, we'll need to place the template in the subDOM tree and also select the elements there too.
-        // We'll attach a shadow root to this element making sure it's in open mode and store the reference to the shadow root in a private variable 
+
+        // We'll attach a shadow root to this element making sure it's in open mode and store the reference to the shadow root in a private variable.
+
+        // Refreshing the component in the browser will show us a shadow root has been added. Now all we need to do is make sure we interact with the shadow root instead of the element's direct contents.
+
+        // This is where we use the root variable. In the browser 
         this._root = this.attachShadow({ "mode": "open" });
     }
     connectedCallback() {
         this._attached = true;
-        this.innerHTML = `
-            <style>
-                .rw-poll-container {
-                    background-color: #333;
-                }
-                .rw-poll-container h3 {
-                    margin: 0;
-                    padding: 0 20px;
-                    color: #FFF;
-                    line-height: 50px;
-                }
-                .rw-poll-container ul {
-                    list-style: none;
-                    margin: 0;
-                    padding: 0;
-                }
-                .rw-poll-container ul li {
-                    padding: 0 20px;
-                    line-height: 50px;
-                    background-color: #E1E1E1;
-                    border: solid 1px #CCC;
-                    border-top: none;
-                    cursor: pointer;
-                }
-                .rw-poll-container ul li:hover {
-                    background-color: #CCC;
-                }
-                .rw-poll-container ul li.selected {
-                    background-color: #5cb85c;
-                    color: #FFF;
-                }
-            </style>
-            <div class="rw-poll-container">
-                <h3 id="question"></h3>
-                <ul id="answers"></ul>
-            </div>          
+
+        this._root.innerHTML = `
+        <style>
+        .rw-poll-container {
+            background-color: #333;
+        }
+        .rw-poll-container h3 {
+            margin: 0;
+            padding: 0 20px;
+            color: #FFF;
+            line-height: 50px;
+        }
+        .rw-poll-container ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+        }
+        .rw-poll-container ul li {
+            padding: 0 20px;
+            line-height: 50px;
+            background-color: #E1E1E1;
+            border: solid 1px #CCC;
+            border-top: none;
+            cursor: pointer;
+        }
+        .rw-poll-container ul li:hover {
+            background-color: #CCC;
+        }
+        .rw-poll-container ul li.selected {
+            background-color: #5cb85c;
+            color: #FFF;
+        }
+        </style>
+        <div class="rw-poll-container">
+        <h3 id="question"></h3>
+        <ul id="answers"></ul>
+        </div>          
         `;
-        this._$question = this.querySelector("#question");
-        this._$answers = this.querySelector("#answers");
+
+        // 3. In the browser we can see that our template is now in the Shadow root. Adding some data will also place the content in the Shadow root, because we updated the references to the questions and answer elements. 
+
+        // Now in the browser we'll notice that our list items now have the desired background color because the shadow root of our work component won't inherit the main document styles giving us complete control over what our component looks like.
+        
+        this._$question = this._root.querySelector("#question");
+        this._$answers = this._root.querySelector("#answers");
         this._$answers.addEventListener("click", (event) => {
             this._$answers.querySelectorAll("li").forEach(($li, index) => {
                 if ($li === event.target) {
@@ -95,22 +106,11 @@ class RwPoll extends HTMLElement {
     get selected() {
         return this._selected;
     }
-
-    // 1. To define a property and a custom element, we will use setters and getters. We'll start by adding a data setter. 
-    // A setter takes only a single argument which is the value being set on a property. 
     set data(data) {
-
-        // 2. As a rule, always chech to see if the data being provided is actually new to avoid doing needless work.
-        if (this._data === data) return;
-
-        // 3. If there is something new instill the data to a private variable 
+        if (this._data === data) return; 
         this._data = data;
-
-        // 4. And then call the render method to update the template with the new data. 
         this._render();
     }
-
-   
     get data() {
         return this._data;
     }
