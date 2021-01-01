7eb6bc2dee86288dc3aaeca02ede3f51fe4045c9
elevalgue <elena.valenzuela.guerrero@gmail.com>
2021-01-01 18:31:36 +0100

    Add disable attribute in the browser

--- 02-creating-star-rating-component/02-implementing-the-disabled-mode/README.md
+++ 02-creating-star-rating-component/02-implementing-the-disabled-mode/README.md
@@ -1,3 +1,7 @@
 # **Implementing the Disabld Mode**
 
 ## **In this demo:**
+
+- We'll use some CSS to prevent hover and change the user's cursor.
+- We'll utilize a private variable to keep track of whether the element is disabled or not.
+- And we'll watch the attribute changes so we know when the element should be put in put in disabled mode.
--- 02-creating-star-rating-component/02-implementing-the-disabled-mode/evg-star-rating.js
+++ 02-creating-star-rating-component/02-implementing-the-disabled-mode/evg-star-rating.js
@@ -4,9 +4,12 @@ class EvgStarRating extends HTMLElement{
         this._root = this.attachShadow({ mode: 'open' });
         this._$stop = null;
         this._$bottom = null;
+        // 1. We'll create a private disable variable
+        this._disabled = null; 
     }  
     connectedCallback() {
-   
+   // Line 60 - The CSS we added first resets the cursor so the user doesn't have to think they're able to click the element in disabled mode. Then it just cancels out the display and the none property in line 41 because we want the top layer of stars to be visible on hover for disabled mode, and it also cancels out the hover effect of the bottom stars. 
+    // Using the host selector along with brackets, we can specify the disable to distribute, which means these CSS properties will only get apllied when our custom element has a disabled a ttribute.  
         this._root.innerHTML =`
         <style>
         :host {
@@ -54,7 +57,8 @@ class EvgStarRating extends HTMLElement{
         .container .bottom > span:hover,
         .container .bottom > span:hover ~ span {               
            color: #e7bd06;
-        }               
+        }    
+              
         :host([disabled]) .container {
             cursor: inherit;
         }
@@ -75,6 +79,9 @@ class EvgStarRating extends HTMLElement{
             </div>
         </div>
     `;
+        // 2. In the connectedcallback method, we'll set this variable to true if the disabled attribute is present. Otherwise, it ill be false. 
+        // We aldo need to observe changes of the disabled attribute so it can update this variable if the desabled attribute is added or removed at one time. 
+        this._disabled = (this.getAttribute('disabled') !== null);
     }
 }
 window.customElements.define('evg-star-rating', EvgStarRating)
