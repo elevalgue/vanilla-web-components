9343487757fdf65a8eea690978729b7e8ea99bc3
elevalgue <elena.valenzuela.guerrero@gmail.com>
2021-01-01 17:45:32 +0100

    Use :host pseudo class which allows us to select a custom element from inside its shadow DOM. Add positioning styling and hover effects

--- 02-creating-star-rating-component/01-creating-the-foundation-of-the-star/evg-star-rating.js
+++ 02-creating-star-rating-component/01-creating-the-foundation-of-the-star/evg-star-rating.js
@@ -17,10 +17,67 @@ class EvgStarRating extends HTMLElement{
     // We add a dataset-value for each of the bottom stars so when one is selected by the user, it's easy to get the selected value. 
     // You may have also noticed that these values are in the wrong order, but we'll explain this shortly.
     connectedCallback() {
+    // 8. Moving on to the CSS, withing a shadow root, we can use the host selector, which refers to the hosts of the shadow root. In this case, that's our star rating element. Thi s means that we can aplly styles directly to the custom element from within the shadow root. 
+    // Next we add some basic styling to position the container and its sections correctly, and finlally we add the hover effect. 
+
         this._root.innerHTML =`
         <style>
-    
-        
+        :host {
+            width: 4.1em;
+            height: 1em;
+            display: inline-block;
+            overflow: hidden;
+            user-select: none;
+            vertical-align: middle;
+            box-sizing: border-box;
+        }          
+        .container {                  
+          color: #c5c5c5;
+          font-size: 1em;
+          line-height: 1em;
+          margin: 0 auto;
+          position: relative;
+          padding: 0;
+          cursor: pointer;
+        }               
+        .container .top {
+          color: #e7bd06;
+          padding: 0;
+          position: absolute;
+          z-index: 1;
+          display: block;
+          top: 0;
+          left: 0;
+          overflow: hidden;
+          width: 0;       
+        }
+        .container:hover .top {
+            display: none;
+        }                             
+        .container .bottom {
+          padding: 0;
+          display: block;
+          position: absolute;
+          top: 0;
+          left: 0;
+          unicode-bidi: bidi-override;
+          direction: rtl;
+        }
+        /* Credit: https://css-tricks.com/star-ratings/ */
+        .container .bottom > span:hover,
+        .container .bottom > span:hover ~ span {               
+           color: #e7bd06;
+        }               
+        :host([disabled]) .container {
+            cursor: inherit;
+        }
+        :host([disabled]) .container .top {
+            display: block;
+        }
+        :host([disabled]) .container .bottom > span:hover,
+        :host([disabled]) .container .bottom > span:hover ~ span {
+            color: inherit;
+        }
         </style>
         <div class="container">
             <div class="top">
