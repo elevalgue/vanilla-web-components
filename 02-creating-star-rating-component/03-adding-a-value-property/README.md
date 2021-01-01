064ab0f361e8dc89a1373a67b4bec3157eb70628
elevalgue <elena.valenzuela.guerrero@gmail.com>
2021-01-02 00:06:45 +0100

    Stablish getters & setters methods to upadte the template and reflect the new value

--- 02-creating-star-rating-component/03-adding-a-value-property/README.md
+++ 02-creating-star-rating-component/03-adding-a-value-property/README.md
@@ -1,3 +1,9 @@
 # **Adding a Value Property**
 
+- Next up is the ability to programmatically set and get the component value. We'll use a **private variable to hold the data**.
+
+- We'll create **setter and getter** methods to **set and retrieve the data**.
+
+- We'll create **render** methods to **update the template to reflect the new value**.
+
 ## **In this demo:**
--- 02-creating-star-rating-component/03-adding-a-value-property/evg-star-rating.js
+++ 02-creating-star-rating-component/03-adding-a-value-property/evg-star-rating.js
@@ -1,11 +1,27 @@
 class EvgStarRating extends HTMLElement{
     constructor() {
         super();
+        // Shadow Root
         this._root = this.attachShadow({ mode: 'open' });
+        // Elements
         this._$stop = null;
         this._$bottom = null;
+        // Data
         this._disabled = null; 
+        // 1. Start out by declaring the private value variable, which will hold the element in its current value.
+        this._value = 0;
     }  
+    // 4. The setter method, as always, will first check to see if the value provided is actually different to the current value. If it is, update the current value and then call the render method to update the template. 
+    set value(value) {
+        if (this._value === value) return;
+        this._value = value;
+        this._render();
+    }
+    // 5. The getter method simply returno the current value. 
+    get value() {
+        return this._value;
+    }
+    
     connectedCallback() {
         this._root.innerHTML =`
         <style>
@@ -77,6 +93,26 @@ class EvgStarRating extends HTMLElement{
         </div>
     `;
         this._disabled = (this.getAttribute('disabled') !== null);
+        // 2. Then, in the connector callback, we'll create a reference to the top element, so we can update its width within the render method. We do that by simply using the query selector method on the reference to the shadow root to pull out the element with the top class. 
+        this._$stop = this._root.querySelector('.top')
+    }
+
+    // 3. The render method is a custom class method which simply takes the value, multiplies it by ten, then doubles it to give us the required width percentage. And using the style property, you can set the width of the top element accordingly.  
+    _render() {
+        if (this._$stop !== null) {
+            this._$stop.style.width = ((this._value * 10) * 2) + '%';
+        }
+    }
+    static get observedAttributes() {
+        return ['disabled'];
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue !== newValue) {
+            switch (name) {
+                case 'disabled':
+                    this._disabled = (newValue !== null);
+            }
+        }
     }
 }
 window.customElements.define('evg-star-rating', EvgStarRating)
