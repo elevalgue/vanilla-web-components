3e5a608693427ba35e2270ae26ccf91c0524e526
elevalgue <elena.valenzuela.guerrero@gmail.com>
2021-01-02 21:11:32 +0100

    Set the CSS for the frame element

--- 03-creating-configurable-slide-out-menu-component/01-creating-foundation-slide-out-menu-web-component/rw-slide-menu.js
+++ 03-creating-configurable-slide-out-menu-component/01-creating-foundation-slide-out-menu-web-component/rw-slide-menu.js
@@ -19,10 +19,28 @@ class RwSlideMenu extends HTMLElement {
     // Next the title which contains a content area and a close button
     // and finally, the menu content area and for the moment here we'll add some basic static content. 
 
-    // 7. 
+    // 7. Moving on to the CSS, we'll first add some for the frame element.
+    // This CSS ensures the frame covers the entire viewport, 
+    // sets pointer-events to none, so the user can click through it,/ 
+    // sets a high z - index, so it's all above all other elements, 
+    // and configures a background-color animation used for the backdrop.// Next is the nav element, which has the container class.We use the translateX transform to slide if off the screen by default, 
+    // and use the transition to ensure the sliding effect is animated
+
     connectedCallback() {
         this._root.innerHTML = `
 
+        <style>
+        .frame {
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            width: 100%
+            overflow: hidden;
+            pointer-events: none;
+            z-index: 1000;
+            transition: background-color 300ms ease-in;
+        }
+        </style>
         
         <div class="frame">
             <nav class="container">
