4c894aceec9e4aed13bf5dde78e447c993802a4c
elevalgue <elena.valenzuela.guerrero@gmail.com>
2021-01-02 21:00:08 +0100

    Stamp an initial template into the Shadow Root

--- 03-creating-configurable-slide-out-menu-component/01-creating-foundation-slide-out-menu-web-component/rw-slide-menu.js
+++ 03-creating-configurable-slide-out-menu-component/01-creating-foundation-slide-out-menu-web-component/rw-slide-menu.js
@@ -12,6 +12,34 @@ class RwSlideMenu extends HTMLElement {
         // 5. and we'll also create a private variable which will hold the open state. 
         this._open = false; 
     }
+
+    // 6. In the connectedCallback method we'll stamp a initial template into the Shadow Root. 
+    //In here we'll add the frame element, which cover the entire browser viewport. 
+    // Then a nav element with a container class
+    // Next the title which contains a content area and a close button
+    // and finally, the menu content area and for the moment here we'll add some basic static content. 
+
+    // 7. 
+    connectedCallback() {
+        this._root.innerHTML = `
+
+        
+        <div class="frame">
+            <nav class="container">
+                <div class="title">
+                    <div class="title-content">
+                        Menu 
+                    </div>
+                    <a class="close">&#10006;</a>
+                </div>
+                <div class="content>
+                    <a href="#">Menu Item One</a>
+                </div>
+            </nav>
+        </div>
+        `;
+    
+    }
 }
 
 // 2. Defined the custom element
