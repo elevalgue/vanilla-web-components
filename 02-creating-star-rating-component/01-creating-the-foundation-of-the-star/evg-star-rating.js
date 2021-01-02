59afe335892c5d4c851f26feb5c98dc7cd459c64
elena valenzuela <elena.valenzuela.guerrero@gmail.com>
2021-01-02 01:10:29 +0100

    Update evg-star-rating.js

--- 02-creating-star-rating-component/01-creating-the-foundation-of-the-star/evg-star-rating.js
+++ 02-creating-star-rating-component/01-creating-the-foundation-of-the-star/evg-star-rating.js
@@ -1,22 +1,30 @@
 // 1. We'll start off by creating our class called EvgStarRating which extends the HTML element interface.
+
 class EvgStarRating extends HTMLElement{
+    
     // 3. The constructor will create a private variable. 
     constructor() {
         super();
+        
         // 4. Storing a reference to the shadow root...
         this._root = this.attachShadow({ mode: 'open' });
+        
         // 5. will create for this element.
         // 6. And also some private variables for important template element references 
+        
         this._$stop = null;
         this._$bottom = null;
     }  
+    
     // 7. Next up, within the connectedCallback we'll stamp an initial template.
     // We have a container with a top and bottom section.
     // The top section holds the gold stars which will become visible when a value is set by setting an appropiate width on this element.
     // The botton section holds the gray stars underneath.
     // We add a dataset-value for each of the bottom stars so when one is selected by the user, it's easy to get the selected value. 
     // You may have also noticed that these values are in the wrong order, but we'll explain this shortly.
+    
     connectedCallback() {
+        
     // 8. Moving on to the CSS, withing a shadow root, we can use the host selector, which refers to the hosts of the shadow root. In this case, that's our star rating element. Thi s means that we can aplly styles directly to the custom element from within the shadow root. 
     // Next we add some basic styling to position the container and its sections correctly, and finlally we add the hover effect.
 
@@ -30,7 +38,8 @@ class EvgStarRating extends HTMLElement{
             user-select: none;
             vertical-align: middle;
             box-sizing: border-box;
-        }          
+        }    
+
         .container {                  
           color: #c5c5c5;
           font-size: 1em;
@@ -39,7 +48,8 @@ class EvgStarRating extends HTMLElement{
           position: relative;
           padding: 0;
           cursor: pointer;
-        }               
+        }  
+
         .container .top {
           color: #e7bd06;
           padding: 0;
@@ -51,9 +61,11 @@ class EvgStarRating extends HTMLElement{
           overflow: hidden;
           width: 0;       
         }
+
         .container:hover .top {
             display: none;
-        }                             
+        }    
+
         .container .bottom {
           padding: 0;
           display: block;
@@ -63,21 +75,26 @@ class EvgStarRating extends HTMLElement{
           unicode-bidi: bidi-override;
           direction: rtl;
         }
+
         /* Credit: https://css-tricks.com/star-ratings/ */
         .container .bottom > span:hover,
         .container .bottom > span:hover ~ span {               
            color: #e7bd06;
-        }               
+        }   
+
         :host([disabled]) .container {
             cursor: inherit;
         }
+
         :host([disabled]) .container .top {
             display: block;
         }
+
         :host([disabled]) .container .bottom > span:hover,
         :host([disabled]) .container .bottom > span:hover ~ span {
             color: inherit;
         }
+
         </style>
         <div class="container">
             <div class="top">
